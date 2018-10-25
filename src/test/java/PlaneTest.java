f71eb39148e9f093ad37504455eb0e84a6015561
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-10-25 14:49:27 +0100

    first commit

---
+++ src/test/java/PlaneTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaneTest {
+
+
+    Plane plane;
+
+    @BeforeEach
+    void init(){
+        plane = new Plane();
+    }
+
+    @Test
+    void PlaneToCreateInstanceOfPlane()
+    {
+        assertThat(plane, instanceOf(Plane.class));
+    }
+}
